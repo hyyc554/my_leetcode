a13a87f4c9e705b0b436fe2c6196f6232c9af03d
黄咏驰 <huangyongchi@yaoguang.net>
2019-05-09 08:49:48 +0800

    归并一些暂时没有处理的记录

--- binary_search/33.py
+++ binary_search/33.py
@@ -21,25 +21,25 @@ class Solution:
             mid = (low + high) // 2
             if target == nums[mid]:
                 return mid
-            if nums[low] <= nums[mid]:                  # 当前全序列已经是正序
-                if nums[low] <= target <= nums[mid]:    # 目标值在最低值与中间值之间
-                    high = mid - 1                      # 最高索引变为中间值-1位
-                else:                                   # 目标值在中间值与最高值之间
-                    low = mid + 1                       # 最低索引变为中间索引+1
-            else:                                       # 当前序列存在反序列
-                if nums[mid] <= target <= nums[high]:   # 目标值在中间值与最高值之间
-                    low = mid + 1                       # 最低索引变为中间值+1位
+            if nums[low] <= nums[mid]:  # 当前全序列已经是正序
+                if nums[low] <= target <= nums[mid]:  # 目标值在最低值与中间值之间
+                    high = mid - 1  # 最高索引变为中间值-1位
+                else:  # 目标值在中间值与最高值之间
+                    low = mid + 1  # 最低索引变为中间索引+1
+            else:  # 当前序列存在反序列
+                if nums[mid] <= target <= nums[high]:  # 目标值在中间值与最高值之间
+                    low = mid + 1  # 最低索引变为中间值+1位
 
-                else:                                   # 目标值在最低值与中间值之间
-                    high = mid - 1                      # 最高索引变为中间值-1位
+                else:  # 目标值在最低值与中间值之间
+                    high = mid - 1  # 最高索引变为中间值-1位
         return -1
 
 
 if __name__ == '__main__':
     a = Solution()
-    nums = [9,8,2,3,4,6,7]
+    nums = [9, 8, 2, 3, 4, 6, 7]
     target = 3
-    b =a.search(nums,target)
+    b = a.search(nums, target)
     print(b)
 
 
