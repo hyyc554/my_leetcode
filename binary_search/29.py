c11f7f87bd795b67c16089fb23d510ad5db5f96f
hyyc554 <1415940604@qq.com>
2020-01-08 20:12:01 +0800

    29

---
+++ binary_search/29.py
@@ -0,0 +1,25 @@
+"""
+还是不太理解明天再回头看看
+"""
+class Solution:
+# @return an integer
+    def divide(self, dividend, divisor):
+        positive = (dividend < 0) is (divisor < 0)
+        dividend, divisor = abs(dividend), abs(divisor)
+        res = 0
+        while dividend >= divisor:
+            temp, i = divisor, 1
+            while dividend >= temp:
+                dividend -= temp
+                res += i
+                i <<= 1
+                temp <<= 1
+        if not positive:
+            res = -res
+        return min(max(-2147483648, res), 2147483647)
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    a = sol.divide(9,2)
+    print(a)
